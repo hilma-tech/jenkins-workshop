59dbbde8dda590cd104132de7478a5766f12edab
Tehila <tehilla.sharbaf@carmel6000.amitnet.org>
2023-06-25 18:32:08 +0300

    added client

---
+++ server/main.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.createServer(app);
+const bodyParser = require('body-parser');
+
+const cors = require("cors")
+
+const PORT = Number(process.env.PORT) || 8000;
+app.use(cors())
+app.use(bodyParser.json())
+
+
+app.get('/api/health', (req, res) => {
+    res.sendStatus(200)
+})
+
+app.post("/api/get-answer", (req, res) => {
+    console.log(req.body);
+    res.send("i do :)")
+})
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
+
